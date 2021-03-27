17de0038a62de50299ff9961ce985a3f738c1d00
Florian Thiévent <florianjeremias@gmail.com>
2021-03-27 22:36:22 +0100

    Finished implemetation

--- src/main/java/RSAMain.java
+++ src/main/java/RSAMain.java
@@ -4,14 +4,17 @@ import ch.fhnw.mada.fthievent.Generate;
 
 /**
  * RSA Implementierung von Florian Thiévent
- *
+ * <p>
  * Verwendung:
  * - Sollen beide Schlüsselpaare generiert, ein Text verschlüsselt und wieder entschlüsselt werden, müssen alle Teile ausgeführt werden
- * - Soll ein Chiffre Text mit einem Schlüssel entschlüsselt werden, müssen die Files in den root kopiert werden als chiffre.txt und sk.txt. Dann die Zeilen 14 - 18 auskommentieren.
+ * - Soll ein Chiffre Text mit einem Schlüssel entschlüsselt werden, müssen die Files in den root kopiert werden als chiffre.txt und sk.txt. Dann die Zeilen 18-22 auskommentieren.
  * - Das Programm arbeitet mit 2048 Bit Schlüsseln. Es ist möglich, dass im Generate Konstrukor eine ander Bit Länge angegeben wird.
  */
 public class RSAMain {
     public static void main(String[] args) throws Exception {
+
+        long startTime = System.currentTimeMillis();
+
         Generate teil1 = new Generate();
         teil1.generate();
 
@@ -20,6 +23,9 @@ public class RSAMain {
 
         Decryptor teil3 = new Decryptor();
         teil3.decrypt();
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("RSA Implementierung brauchte " + (endTime - startTime) + " millisekunden.");
 
     }
 }
